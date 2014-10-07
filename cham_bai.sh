d1423ae88754b4a291e57ba645d2de9dbcb63d78
pntruongan <pntruongan@gentoopc>
2014-10-08 01:54:16 +0700

    cham bai

--- cham_bai.sh
+++ cham_bai.sh
@@ -26,15 +26,18 @@ do
 	#cat $i;
 	answer=$(cat ${i%.*}.ans)
 	output=$(timeout 15 bash -c "cat $i | $binary")
-	trimmed=$(echo $output |  awk '{print $(NF-1) " " $NF}' | sed 's/[^0-9 ]//g')
+	#trimmed=$(echo $output |  awk '{print $(NF-1) " " $NF}' | sed 's/[^0-9 -]//g')
 	#trimmed=$(echo $output |  awk '{print $NF}' | sed 's/[^0-9]//g')
+	trimmed=$output
 	if [ "$answer" = "$trimmed" ] 
 	then
 		let "count=count+1"
 	else
 		#cat $i
 		echo $i;
-		echo "answer = $answer BUT output = $trimmed"
+		#echo "answer = $answer BUT output = $trimmed"
+		#echo "answer = $answer BUT output = $output"
+		diff <(echo "$output")  <(echo "$answer")
 		flag=0
 	fi
 done
